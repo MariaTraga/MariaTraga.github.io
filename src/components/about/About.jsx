e1edbc2cc9da043d8ea3b333a2fbb56a16fc6e50
MariaTraga <maria.traga@outlook.com>
2021-12-20 01:50:27 +0200

    changed image/ fixed scroll on smartphones

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,5 @@
 import "./About.css";
+import Me1 from "../../images/me1.jpg";
 
 const About = () => {
     return (
@@ -10,16 +11,17 @@ const About = () => {
                 <div className="about-card">
                     <img 
                         className="about-image" 
-                        src="https://www.bandt.com.au/information/uploads/2020/09/iStock-1170073827-1260x840.jpg"/>
+                        src={Me1}/>
                 </div>
             </div>
             <div className="about-right">
                 <h1 className="about-title">About Me</h1>
                 <p className="about-sub">
-                    My name is giorno giovanna
+                    My name is Maria Traga. 
                 </p>
                 <p className="about-desc">
-                    Lorem ipsum
+                    I have studied Computer Science for my Master's Degree and Business Administration in my Bachelor's Degree.<br/> 
+                    I develop web apps and I am very entusiastic about game development. 
                 </p>
             </div>
         </div>
